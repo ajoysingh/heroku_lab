555ca27c0d755842ba821eb18d700aa7049bab30
Ajoy Singh <Ajoy Singh>
2017-04-21 21:11:43 -0400

    Updated port information 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 app.get('/', function(req, res) {	
    res.send('Hello World @\n' + req.connection.remoteAddress);
 });
-var port = process.env.PORT || 8080)
+var port = process.env.PORT || 8080;
 
 app.listen(port);
 console.log('Server listening at port: ' + port);
